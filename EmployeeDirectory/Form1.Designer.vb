d045b17827ea48f1e852711faccec7c748b0a2c3
MERCURY\LS004313131 <LeahMarra.Soltes@alorica.com>
2018-01-10 23:04:59 +0800

    Rename Form and Add Marra Button

--- EmployeeDirectory/Form1.Designer.vb
+++ EmployeeDirectory/Form1.Designer.vb
@@ -22,9 +22,29 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.MarraButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'MarraButton
+        '
+        Me.MarraButton.Location = New System.Drawing.Point(110, 199)
+        Me.MarraButton.Name = "MarraButton"
+        Me.MarraButton.Size = New System.Drawing.Size(75, 23)
+        Me.MarraButton.TabIndex = 0
+        Me.MarraButton.Text = "MARRA"
+        Me.MarraButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.MarraButton)
+        Me.Name = "Form1"
+        Me.Text = "Test Source Tree"
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents MarraButton As System.Windows.Forms.Button
 
 End Class
