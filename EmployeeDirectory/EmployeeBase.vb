b33223648693cff7b33a70f882cfd92682134ef8
Robert John Millano <bemanj87@gmail.com>
2018-01-10 22:15:20 +0800

    employee base

--- EmployeeDirectory/EmployeeBase.vb
+++ EmployeeDirectory/EmployeeBase.vb
@@ -1,13 +1,3 @@
 ﻿Public Class EmployeeBase
-    Private _lastname As String
-    Public Property LastName() As String
-        Get
-            Return _lastname
-        End Get
-        Set(ByVal value As String)
-            _lastname = value
-        End Set
-    End Property
-
 
 End Class
