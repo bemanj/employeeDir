6b8d97fe86e5cbf825f25caad59874beeab0278e
Robert John Millano <bemanj87@gmail.com>
2018-01-10 22:11:38 +0800

    add employee

---
+++ EmployeeDirectory/EmployeeBase.vb
@@ -0,0 +1,13 @@
+﻿Public Class EmployeeBase
+    Private _lastname As String
+    Public Property LastName() As String
+        Get
+            Return _lastname
+        End Get
+        Set(ByVal value As String)
+            _lastname = value
+        End Set
+    End Property
+
+
+End Class
